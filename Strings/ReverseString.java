2398fe96a53733c45730934f48a61462a6be2cbd
Anshika <agarwalanshika226@gmail.com>
2026-08-18 01:31:41 +0530

    Add Reverse String solution

---
+++ Strings/ReverseString.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class ReverseString {
+    class Solution {
+        public void reverseString(char[] s) {
+            char temp;
+            int i=0; 
+                int j=s.length-1;
+            while(i<=j){
+                temp =s[i];
+                s[i]= s[j];
+                s[j]= temp;
+                i++;
+                j--;
+            }
+            System.out.println(s);
+        }
+    }
+}
