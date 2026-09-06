8a14d29a58be267835c1901c8b0887cb0ebc457d
Anshika <agarwalanshika226@gmail.com>
2026-09-06 20:09:22 +0530

    Add Maximum Subarray solution

---
+++ Arrays/MaximumSubarray.java
@@ -0,0 +1,12 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxsum =nums[0];
+        int currentsum =nums[0];
+        for(int i =1; i<nums.length; i++){
+            currentsum = Math.max(nums[i], nums[i]+currentsum);
+            maxsum =Math.max(maxsum, currentsum);
+            
+        }
+        return maxsum ;
+    }
+}
