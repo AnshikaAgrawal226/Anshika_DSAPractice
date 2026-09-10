0797551e4214af0fdb1301e86ece21566b66822c
Anshika <agarwalanshika226@gmail.com>
2026-09-10 23:52:01 +0530

    Add Two Sum II solution

---
+++ Arrays/TwoSumII.java
@@ -0,0 +1,22 @@
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int start =0; 
+        int end = numbers.length-1;
+        while(start<end){
+            int sum = numbers[start]+ numbers[end];
+            if(target==sum){
+                ans[0]= start+1;
+                ans[1] = end+1;
+                return ans;
+            }
+            else if(sum>target){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
