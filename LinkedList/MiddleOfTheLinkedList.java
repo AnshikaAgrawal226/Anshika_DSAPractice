2f43d2b5683109e1ea65083a2ee63415e7797e9c
Anshika <agarwalanshika226@gmail.com>
2026-09-15 20:28:30 +0530

    Add Reverse LinkedList and Midlle of LinkedList solution in LinkedList Folder

---
+++ LinkedList/MiddleOfTheLinkedList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class MiddleOfTheLinkedList {
+    
+ //Definition for singly-linked list.
+  public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) { 
+            this.val = val; this.next = next; 
+        }
+  }
+ 
+
+    public ListNode middleNode(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast  =head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
